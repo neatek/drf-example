9d3f84791d00ccbed1a5637e427ac29d662e2202
Vladimir <neatek@icloud.com>
2022-12-31 00:57:31 +0400

    Add get request (axios.js) for DRF API

--- library/frontend/src/App.js
+++ library/frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios'
 // import logo from './logo.svg';
 import './App.css';
 import AuthorList from './components/Author.js'
@@ -12,23 +13,15 @@ class App extends React.Component {
     }
   }
   componentDidMount() {
-    const authors = [
-      {
-        'first_name': 'Фёдор',
-        'last_name': 'Достоевский',
-        'birthday_year': 1821
-      },
-      {
-        'first_name': 'Александр',
-        'last_name': 'Грин',
-        'birthday_year': 1880
-      },
-    ]
-    this.setState(
-      {
-        'authors': authors
-      }
-    )
+    axios.get('http://127.0.0.1:8000/api/authors')
+      .then(response => {
+        const authors = response.data
+        this.setState(
+          {
+            'authors': authors
+          }
+        )
+      }).catch(error => console.log(error))
   }
   render() {
     return (
